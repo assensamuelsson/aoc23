5c66b7147b4d3e8c7a76ae1e5538a2aac4899f8b
Andreas Samuelsson <andreas.samuelsson@bonniernews.se>
2023-12-01 20:19:34 +0100

    Day 1

--- src/day1/src.mjs
+++ src/day1/src.mjs
@@ -1,9 +1,75 @@
 export const part1 = (input) => {
-  const result = parse(input);
-  console.log(`Part 1: ${result}`);
+  const result = input.split('\n')
+    .map(getCalibrationValues)
+    .map((value) => parseInt(value))
+    .reduce((acc, value) => acc + value, 0);
+
+  console.log('Part1:', result);
 }
 
-export const parse = (input) => input
-  .split('\n')
-  .map((x) => parseInt(x, 10))
-  .reduce((a, b) => a + b, 0);
+export const part2 = (input) => {
+  const result = input.split('\n')
+    .map(getCalibrationValues2)
+    .map((value) => parseInt(value))
+    .reduce((acc, value) => acc + value, 0);
+
+  console.log('Part2:', result);
+}
+
+export const getCalibrationValues = (input) => {
+  let first, last;
+  for (let i = 0; i < input.length; i++) {
+    const char = input[i];
+    const parsed = parseInt(char, 10);
+    if (parsed >= 0 && parsed <= 9) {
+      if (first === undefined) {
+        first = char;
+      }
+      last = char;
+    }
+  }
+  return first + last;
+}
+
+export const getNumberTokens = (input) => {
+  const numberTokens = [];
+  const patterns = [
+    { pattern: '1', meaning: '1'},
+    { pattern: '2', meaning: '2'},
+    { pattern: '3', meaning: '3'},
+    { pattern: '4', meaning: '4'},
+    { pattern: '5', meaning: '5'},
+    { pattern: '6', meaning: '6'},
+    { pattern: '7', meaning: '7'},
+    { pattern: '8', meaning: '8'},
+    { pattern: '9', meaning: '9'},
+    { pattern: 'one', meaning: '1'},
+    { pattern: 'two', meaning: '2'},
+    { pattern: 'three', meaning: '3'},
+    { pattern: 'four', meaning: '4'},
+    { pattern: 'five', meaning: '5'},
+    { pattern: 'six', meaning: '6'},
+    { pattern: 'seven', meaning: '7'},
+    { pattern: 'eight', meaning: '8'},
+    { pattern: 'nine', meaning: '9'},
+  ];
+
+  for (const { pattern, meaning } of patterns) {
+    const first = input.indexOf(pattern)
+    if (first !== -1) {
+      numberTokens.push([first, meaning])
+    }
+    const last = input.lastIndexOf(pattern)
+    if (last !== -1 && last !== first) {
+      numberTokens.push([last, meaning])
+    }
+  }
+
+  return numberTokens;
+}
+
+export const getCalibrationValues2 = (input) => {
+  const tokens = getNumberTokens(input);
+  tokens.sort((a, b) => parseInt(a[0]) - parseInt(b[0]))
+  return tokens[0][1] + tokens[tokens.length - 1][1]; 
+}
--- src/day1/test.mjs
+++ src/day1/test.mjs
@@ -1,11 +1,31 @@
-import assert from 'node:assert';
+import { strictEqual, deepStrictEqual } from 'node:assert';
 import { it, describe } from 'node:test';
 
-import { parse } from './src.mjs';
+import {
+  getCalibrationValues,
+  getNumberTokens,
+  getCalibrationValues2,
+ } from './src.mjs';
 
 describe('day1', () => {
-  it('should pass', () => {
-    const input = '1\n2\n3\n4\n5\n6\n7\n8\n9';
-    assert.strictEqual(parse(input), 45);
+  it('Gets correct calibration values', () => {
+    strictEqual(getCalibrationValues('1abc2'), '12');
+    strictEqual(getCalibrationValues('pqr3stu8vwx'), '38');
+    strictEqual(getCalibrationValues('a1b2c3d4e5f'), '15');
+    strictEqual(getCalibrationValues('treb7uchet'), '77');
+  });
+
+  it('Extracts correct number tokens', () => {
+    deepStrictEqual(getNumberTokens('1a3'), [[0, '1'], [2, '3']]);
+    deepStrictEqual(getNumberTokens('113'), [[0, '1'], [1, '1'], [2, '3']]);
+    deepStrictEqual(getNumberTokens('1abc2'), [[0, '1'], [4, '2']]);
+    deepStrictEqual(getNumberTokens('four1abcnine'), [[4, '1'], [0, '4'], [8, '9']]);
+  });
+
+  it('Gets correct calibration values - updated', () => {
+    strictEqual(getCalibrationValues2('two1nine'), '29');
+    strictEqual(getCalibrationValues2('eightwothree'), '83');
+    strictEqual(getCalibrationValues2('4nineeightseven2'), '42');
+    strictEqual(getCalibrationValues2('7pqrstsixteen'), '76');
   });
 })
